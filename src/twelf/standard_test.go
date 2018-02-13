c01da05cfac4df978a0c4a177955625262c7264b
James Harris <james.harris@icecave.com.au>
2018-02-13 12:17:58 +1000

    Add more tests.

--- src/twelf/standard_test.go
+++ src/twelf/standard_test.go
@@ -29,6 +29,28 @@ var _ = Describe("StandardLogger", func() {
 			logger.CaptureDebug = true
 		})
 
+		Describe("Log", func() {
+			It("writes a formatted message to the target", func() {
+				logger.Log("message <%s>", "arg")
+
+				Expect(buffer.String()).To(Equal("message <arg>\n"))
+			})
+		})
+
+		Describe("LogString", func() {
+			It("writes a message to the target", func() {
+				logger.LogString("<message>")
+
+				Expect(buffer.String()).To(Equal("<message>\n"))
+			})
+
+			It("does not substitute placeholders", func() {
+				logger.LogString("<%s>")
+
+				Expect(buffer.String()).To(Equal("<%s>\n"))
+			})
+		})
+
 		Describe("Debug", func() {
 			It("writes a formatted message to the target", func() {
 				logger.Debug("message <%s>", "arg")
@@ -39,9 +61,61 @@ var _ = Describe("StandardLogger", func() {
 
 		Describe("DebugString", func() {
 			It("writes a message to the target", func() {
-				logger.Debug("<message>")
+				logger.DebugString("<message>")
 
 				Expect(buffer.String()).To(Equal("<message>\n"))
+			})
+
+			It("does not substitute placeholders", func() {
+				logger.DebugString("<%s>")
+
+				Expect(buffer.String()).To(Equal("<%s>\n"))
+			})
+		})
+
+		Describe("IsDebug", func() {
+			It("returns true", func() {
+				Expect(logger.IsDebug()).To(BeTrue())
+			})
+		})
+	})
+
+	Context("when debug logging is disabled", func() {
+		Describe("Log", func() {
+			It("writes a formatted message to the target", func() {
+				logger.Log("message <%s>", "arg")
+
+				Expect(buffer.String()).To(Equal("message <arg>\n"))
+			})
+		})
+
+		Describe("LogString", func() {
+			It("writes a message to the target", func() {
+				logger.LogString("<message>")
+
+				Expect(buffer.String()).To(Equal("<message>\n"))
+			})
+		})
+
+		Describe("Debug", func() {
+			It("does not produce any output", func() {
+				logger.Debug("message <%s>", "arg")
+
+				Expect(buffer.String()).To(Equal(""))
+			})
+		})
+
+		Describe("DebugString", func() {
+			It("does not produce any output", func() {
+				logger.DebugString("<message>")
+
+				Expect(buffer.String()).To(Equal(""))
+			})
+		})
+
+		Describe("IsDebug", func() {
+			It("returns false", func() {
+				Expect(logger.IsDebug()).To(BeFalse())
 			})
 		})
 	})
